3d778fd4c1a28dffbf354e3cd5110bc257b57aa6
Alaa Eddine Boulessane <alaaedine.boulessane@laplateforme.io>
2025-11-17 09:34:29 +0100

    vérification de longueur, majuscule et minuscule

--- projet/password.py
+++ projet/password.py
@@ -1,1 +1,16 @@
-def verif():
+def verif(password):
+    majusc = 0
+    minusc = 0
+    long = len(password)
+    if long < 8:
+        return "Mot de passe trop court"
+    for i in range(long):
+        if password[i].isupper():
+            majusc += 1
+        if password[i].islower():
+            minusc +=1
+    if majusc == 0:
+        return "Doit contenir au moins une lettre majuscule"
+    if minusc == 0:
+        return "Doit contenir au moins une lettre minuscule"
+    
