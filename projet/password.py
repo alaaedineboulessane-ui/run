6cc8f3820c7cbd38f67cd651ffd5dd3df36fdf85
Alaa Eddine Boulessane <alaaedine.boulessane@laplateforme.io>
2025-11-17 09:49:30 +0100

    verification de charactere special

--- projet/password.py
+++ projet/password.py
@@ -1,8 +1,10 @@
 def verif(password):
     abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    special = "!@#$%^&*"
     majusc = 0
     minusc = 0
     chiffre = 0
+    spec = 0
     long = len(password)
     if long < 8:
         return "Mot de passe trop court"
@@ -11,14 +13,18 @@ def verif(password):
             majusc += 1
         if password[i].islower():
             minusc +=1
-        if not password[i]  in abc:
+        if not password[i] in abc:
             chiffre += 1
+        if password[i] in special:
+            spec += 1
     if majusc == 0:
         return "Doit contenir au moins une lettre majuscule"
     elif minusc == 0:
         return "Doit contenir au moins une lettre minuscule"
     elif chiffre == 0:
         return "Doit contenir au moins un chiffre"
+    elif spec == 0:
+        return "Doit contenir au moins un charactere special"
     
 
-print(verif("aladinaaaA"))
+print(verif("alad5in%aaaA"))
