78eb78bfd3b2774a6dd57d7c1128523cb04dd199
Alaa Eddine Boulessane <alaaedine.boulessane@laplateforme.io>
2025-11-17 09:46:19 +0100

    verification de chiffre

--- projet/password.py
+++ projet/password.py
@@ -1,6 +1,8 @@
 def verif(password):
+    abc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     majusc = 0
     minusc = 0
+    chiffre = 0
     long = len(password)
     if long < 8:
         return "Mot de passe trop court"
@@ -9,8 +11,14 @@ def verif(password):
             majusc += 1
         if password[i].islower():
             minusc +=1
+        if not password[i]  in abc:
+            chiffre += 1
     if majusc == 0:
         return "Doit contenir au moins une lettre majuscule"
-    if minusc == 0:
+    elif minusc == 0:
         return "Doit contenir au moins une lettre minuscule"
+    elif chiffre == 0:
+        return "Doit contenir au moins un chiffre"
     
+
+print(verif("aladinaaaA"))
