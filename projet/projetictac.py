0930f2df0c97f6943e74f5ae6af8f2e62f23e9be
Alaa Eddine Boulessane <alaaedine.boulessane@laplateforme.io>
2025-11-19 10:16:38 +0100

    Feat : creation d'une fonction choix pour laisser l'internaute choisir son signe, creation d'une fonction espace_vide qui vérifie toutes les listes vides dans une liste, création d'un fonction victoire qui verifie le tableau et dis s'il il y a une victoire, création du tictac qui réuni toutes les fonctions et laisse le jouer un et deux jouer

--- projet/projetictac.py
+++ projet/projetictac.py
@@ -1,15 +1,117 @@
 import random
 
-def tictac(choix):
+def choix(reponse):
     tab = ["X", "O"]
     signe = ""
-    while choix != "X" or choix != "O"
-        if choix == 1:
-            signe = tab[0]
-        elif choix == 2:
-            signe = tab[1]
+    if reponse == 1:
+        return tab[0]
+    elif reponse == 2:
+        return tab[1]
+    else:
+        return tab[random.randint(0,1)]
+
+
+def espace_vide(liste):
+    verif = 0
+    for i in range(len(liste)):
+        if liste[i] == []:
+            verif += 1 
+    return verif
+
+def victoire(liste):
+    if liste[0] == liste[1] and liste[0] == liste[2] and liste[0] != [] and liste[1] != [] and liste[2] != []:
+        return True
+    elif liste[0] == liste[4] and liste[0] == liste[8] and liste[0] != [] and liste[4] != [] and liste[8] != []:
+        return True
+    elif liste[2] == liste[4] and liste[2] == liste[6] and liste[2] != [] and liste[4] != [] and liste[6] != []:
+        return True
+    elif liste[0] == liste[3] and liste[0] == liste[6] and liste[0] != [] and liste[3] != [] and liste[6] != []:
+        return True
+    elif liste[3] == liste[4] and liste[3] == liste[5] and liste[3] != [] and liste[4] != [] and liste[5] != []:
+        return True
+    elif liste[2] == liste[5] and liste[2] == liste[8] and liste[2] != [] and liste[5] != [] and liste[8] != []:
+        return True
+    elif liste[1] == liste[4] and liste[1] == liste[7] and liste[1] != [] and liste[4] != [] and liste[7] != []:
+        return True
+    elif liste[6] == liste[7] and liste[6] == liste[8] and liste[6] != [] and liste[7] != [] and liste[8] != []:
+        return True
+    return False
+        
+
+def tictac(choix):
+    action = ""
+    ind = 1
+    jeu = [[] for i in range(9)]
+    reponse = int(input("Joueur 1 choissisez votre signe (1 pour le signe X, 2 pour le signe O): "))
+    signe = choix(reponse)
+    signe1 = "O" if signe == "X" else "X"
+    ligne_haut = ["haut_gauche", "haut_centre", "haut_droite"]
+    ligne_centre = ["centre_gauche", "centre_centre", "centre_droite"]
+    ligne_bas = ["bas_gauche", "bas_centre", "bas_droite"]
+    print("Voilà les placements possibles, il faut taper exactement ce qui est dit: " + "\n" + ligne_haut + "\n" + ligne_centre + "\n" + ligne_bas)
+    while espace_vide(jeu) != 0:    
+        debut = input("Joueur numéro 1, jouez le tour numéro " + str(ind) + " ! Où voulez-vous placer votre signe ?: ")
+        if debut in ligne_haut or debut in ligne_bas or debut in ligne_centre:
+            if debut == "haut_gauche" and jeu[0] == []:
+                jeu[0] = signe
+            elif debut == "haut_centre" and jeu[1] == []:
+                jeu[1] = signe
+            elif debut == "haut_droite" and jeu[2] == []:
+                jeu[2] = signe
+            elif debut == "centre_gauche" and jeu[3] == []:
+                jeu[3] = signe
+            elif debut == "centre_centre" and jeu[4] == []:
+                jeu[4] = signe
+            elif debut == "centre_droite" and jeu[5] == []:
+                jeu[5] = signe
+            elif debut == "bas_gauche" and jeu[6] == []:
+                jeu[6] = signe
+            elif debut == "bas_centre" and jeu[7] == []:
+                jeu[7] = signe
+            elif debut == "bas_droite" and jeu[8] == []:
+                jeu[8] = signe
+            action = "Joueur 1"
+            if victoire(jeu) == True:
+                for i in range(0, 9, 3):
+                    print(jeu[i:i+3])
+                return "Le " + action + " a gagné !"
         else:
-            signe = tab[random.randint(0,1)]
-            return "Choix indisponible, choix aléatoire attribué" + signe
-    return "Votre signe est " + signe
+            print("Vous avez fait un mauvais choix, la case est deja prise ou n'existe pas, veuillez réssayer")
+        debut1 = input("Joueur numéro 2, jouez le tour numéro " + str(ind) + " ! Où voulez-vous placer votre signe ?: ")
+        if debut1 in ligne_haut or debut1 in ligne_bas or debut1 in ligne_centre:
+            if debut1 == "haut_gauche" and jeu[0] == []:
+                jeu[0] = signe1
+            elif debut1 == "haut_centre" and jeu[1] == []:
+                jeu[1] = signe1
+            elif debut1 == "haut_droite" and jeu[2] == []:
+                jeu[2] = signe1
+            elif debut1 == "centre_gauche" and jeu[3] == []:
+                jeu[3] = signe1
+            elif debut1 == "centre_centre" and jeu[4] == []:
+                jeu[4] = signe1
+            elif debut1 == "centre_droite" and jeu[5] == []:
+                jeu[5] = signe1
+            elif debut1 == "bas_gauche" and jeu[6] == []:
+                jeu[6] = signe1
+            elif debut1 == "bas_centre" and jeu[7] == []:
+                jeu[7] = signe1
+            elif debut1 == "bas_droite" and jeu[8] == []:
+                jeu[8] = signe1
+            action = "Joueur 2"
+            if victoire(jeu) == True:
+                for i in range(0, 9, 3):
+                    print(jeu[i:i+3])
+                return "Le " + action + " a gagné !"
+        else:
+            print("Vous avez fait un mauvais choix, la case est deja prise ou n'existe pas, veuillez réssayer")
+        ind += 1
+        for i in range(0, 9, 3):
+            print(jeu[i:i+3])
+    return "Match nul ! Retentez !"
+
+
+
+                
+
+
 
