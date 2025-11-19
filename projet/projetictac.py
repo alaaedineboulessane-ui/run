9ef83eec0b9b73f749a65ded0042b2f6d3716137
Alaa Eddine Boulessane <alaaedine.boulessane@laplateforme.io>
2025-11-19 08:46:44 +0100

    feat : création d'une fonction pour laisser l'internaute choisir son signe

--- projet/projetictac.py
+++ projet/projetictac.py
@@ -1,1 +1,15 @@
-def tictac():
+import random
+
+def tictac(choix):
+    tab = ["X", "O"]
+    signe = ""
+    while choix != "X" or choix != "O"
+        if choix == 1:
+            signe = tab[0]
+        elif choix == 2:
+            signe = tab[1]
+        else:
+            signe = tab[random.randint(0,1)]
+            return "Choix indisponible, choix aléatoire attribué" + signe
+    return "Votre signe est " + signe
+
